e659666d7843f0ca70335a60fb5493c08ee635a8
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-06-06 11:57:36 -0600

    Working with appsettings, Microsoft.EntityFrameworkCore.Design and tittle changes

--- Program.cs
+++ Program.cs
@@ -3,12 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<JobsContext>(p => p.UseInMemoryDatabase("JobsDB"));
+// builder.Services.AddDbContext<JobsDbContext>(p => p.UseInMemoryDatabase("JobsDB"));
+builder.Services.AddSqlServer<JobsDbContext>(builder.Configuration.GetConnectionString("sql"));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/dbconextion", async ([FromServices] JobsContext dbcontext) =>
+app.MapGet("/dbconextion", async ([FromServices] JobsDbContext dbcontext) =>
 {
     dbcontext.Database.EnsureCreated();
     return Results.Ok("In memory db " + dbcontext.Database.IsInMemory());
