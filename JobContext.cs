a50634f1429499c192a7135cfbc91997bd53f035
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-06-06 12:31:39 -0600

    Correct initial table created when api called like a ping

--- JobContext.cs
+++ JobContext.cs
@@ -5,8 +5,11 @@ namespace EFCore;
 
 public class JobsDbContext : DbContext
 {
-    DbSet<Category> categories;
-    DbSet<Job> jobs;
+    //Previus DbSet<Category> categories
+    //Before  DbSet<Category> categories { get; set; }
+    //Tables was creadted correctly 
+    DbSet<Category> categories { get; set; }
+    DbSet<Job> jobs { get; set; }
 
     public JobsDbContext(DbContextOptions options) : base(options) {
         
